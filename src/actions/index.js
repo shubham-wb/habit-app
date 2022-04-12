ccf0218acfe077bd4870167859dac5279bd515de
shubham-wb <scmotorsofficial@gmail.com>
2022-04-12 19:49:09 +0530

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ export const ADD_HABIT = "ADD_HABIT";
 export const REMOVE_HABIT = "REMOVE_HABIT";
 export const TOGGLE_STATUS = "TOGGLE_STATUS";
 
-// action creators
+// action creator for adding habit to global state
 export function addHabit(habit) {
   return {
     type: ADD_HABIT,
@@ -11,13 +11,19 @@ export function addHabit(habit) {
   };
 }
 
-// action creators
+// action creators for removing habit from global state
 export function removeHabit(habit) {
   return {
     type: REMOVE_HABIT,
     habit,
   };
 }
+
+//action creator to toggle habit status
+//takes three input
+// 1. index of habit of which status is to be changed
+// 2. day of the status to be changed i.e. id of status property
+// 3. value of status 0-->Pending , 1--> Done 2--> Not Done
 
 export function toggleHabitStatus(index, id, value) {
   return {
